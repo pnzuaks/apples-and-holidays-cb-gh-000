0ce7f37d3c0b1e2973de90cc8b27fe82be4a18ce
Patricia Nykaza <pnykaza@calstatela.edu>
2018-04-20 22:16:23 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,9 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.each do |key, val|
     val.each do |holiday|
-      
+      if holiday[array].includes?("BBQ")
+        puts holiday
+    end
   end
   end
 
